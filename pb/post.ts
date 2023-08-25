bb7b1239584d33e0e02b252c16239022c94d5205
buihongquan <buihongquan@vnpt.vn>
2023-08-25 15:39:45 +0700

    add full logic for crud

---
+++ pb/post.ts
@@ -0,0 +1,18 @@
+import type * as grpc from '@grpc/grpc-js';
+import type { MessageTypeDefinition } from '@grpc/proto-loader';
+
+
+type SubtypeConstructor<Constructor extends new (...args: any) => any, Subtype> = {
+  new(...args: ConstructorParameters<Constructor>): Subtype;
+};
+
+export interface ProtoGrpcType {
+  Post: MessageTypeDefinition
+  PostResponse: MessageTypeDefinition
+  google: {
+    protobuf: {
+      Timestamp: MessageTypeDefinition
+    }
+  }
+}
+
---
+++ pb/post/UpdatePostRequest.ts
@@ -0,0 +1,20 @@
+// Original file: proto/rpc_update_post.proto
+
+
+export interface UpdatePostRequest {
+  'id'?: (string);
+  'title'?: (string);
+  'content'?: (string);
+  'category'?: (string);
+  'image'?: (string);
+  'published'?: (boolean);
+}
+
+export interface UpdatePostRequest__Output {
+  'id': (string);
+  'title': (string);
+  'content': (string);
+  'category': (string);
+  'image': (string);
+  'published': (boolean);
+}
